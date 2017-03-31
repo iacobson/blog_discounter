e8c879893c67c76f29fa12b6105291cc98ba9440
iacobson <dorian.iacobescu@gmail.com>
2017-03-31 15:05:55 +0100

    setup umbrella app

---
+++ lib/discounter.ex
@@ -0,0 +1,18 @@
+defmodule Discounter do
+  @moduledoc """
+  Documentation for Discounter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Discounter.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
